3ab4712e7433cb3837c1c5bcf6cdefc29855323d
Aleksandar Dezelin <dezelin@gmail.com>
2016-01-27 17:03:53 +0100

    Implemented AddrMessage Bitcoin protocol message.

---
+++ src/main/java/bitcoin4j/network/protocol/AddrMessage.java
@@ -0,0 +1,84 @@
+/*
+ *
+ * Copyright (c) 2016 Aleksandar Dezelin
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+ * EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * 
+ */
+package bitcoin4j.network.protocol;
+
+/**
+ * @author dezelin
+ *
+ */
+public class AddrMessage implements ControlMessage {
+
+	private static final String commandName = "addr";
+
+	private CompactInteger addrCount;
+	private IPAddress[] addresses;
+
+	/**
+	 * 
+	 */
+	public AddrMessage(CompactInteger addrCount, IPAddress[] addresses) {
+		this.setAddrCount(addrCount);
+		this.setAddresses(addresses);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see bitcoin4j.network.protocol.Message#getCommandName()
+	 */
+	@Override
+	public String getCommandName() {
+		return commandName;
+	}
+
+	/**
+	 * @return the addrCount
+	 */
+	public CompactInteger getAddrCount() {
+		return addrCount;
+	}
+
+	/**
+	 * @param addrCount
+	 *            the addrCount to set
+	 */
+	public void setAddrCount(CompactInteger addrCount) {
+		this.addrCount = addrCount;
+	}
+
+	/**
+	 * @return the addresses
+	 */
+	public IPAddress[] getAddresses() {
+		return addresses;
+	}
+
+	/**
+	 * @param addresses
+	 *            the addresses to set
+	 */
+	public void setAddresses(IPAddress[] addresses) {
+		this.addresses = addresses;
+	}
+
+}
