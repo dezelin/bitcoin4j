21d78884bb40f29c936ce04f4120d56a028c58ba
Aleksandar Dezelin <dezelin@gmail.com>
2016-01-27 17:30:07 +0100

    Implemented PingMessage Bitcoin protocol message.

---
+++ src/main/java/bitcoin4j/network/protocol/PingMessage.java
@@ -0,0 +1,60 @@
+/*
+ *
+ * Copyright (c) 2016 Aleksandar Dezelin
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+ * EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * 
+ */
+package bitcoin4j.network.protocol;
+
+/**
+ * @author dezelin
+ *
+ */
+public class PingMessage implements ControlMessage {
+
+	private static final String commandName = "ping";
+
+	private long nonce;
+
+	/**
+	 * 
+	 */
+	public PingMessage(long nonce) {
+		this.setNonce(nonce);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see bitcoin4j.network.protocol.Message#getCommandName()
+	 */
+	@Override
+	public String getCommandName() {
+		return commandName;
+	}
+
+	public long getNonce() {
+		return nonce;
+	}
+
+	public void setNonce(long nonce) {
+		this.nonce = nonce;
+	}
+
+}
